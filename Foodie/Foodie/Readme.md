a9a3d462cbdb754f172ca7f1b49d35aaddb3e461
Anbalagan D <anbu94p@gmail.com>
2022-08-22 17:32:54 +0530

    Add Source

---
+++ Foodie/Foodie/Readme.md
@@ -0,0 +1,75 @@
+# UNUserNotification Framework Sample Project
+
+## Overview
+
+From iOS 10.0 apple provide UserNotifications frame to handling local and remote notification. 
+
+## Feature
+
+1. Permission
+2. Grouping
+3. Trigger
+4. Attachment and Sound
+5. Category and Action
+6. Service extension
+7. Content extension
+
+
+### Permission
+
+iOS 10.0 introduce new `User Notification` framework to handle both remote and local notification. And support badge, sound, alert type authorization as well.
+
+iOS 12.0 apple introduce new authorization option `provisional`, `providesAppNotificationSettings` and `criticalAlert`
+
+`provisional`: The ability to post noninterrupting notifications provisionally to the Notification Center.
+`providesAppNotificationSettings`: An option indicating the system should display a button for in-app notification settings.
+`criticalAlert`: The ability to play sounds for critical alerts.
+
+
+### Grouping
+
+Set `threadIdentifier` in `UNMutableNotificationContent` system group the notification using this identifier.
+
+
+### Trigger
+
+iOS support four type of Trigger to schedule notification
+
+1. UNCalendarNotificationTrigger
+2. UNTimeIntervalNotificationTrigger
+3. UNLocationNotificationTrigger
+4. UNPushNotificationTrigger
+
+Note: Except `UNPushNotificationTrigger` we can use all other trigger to schedule our notification.
+
+
+### Attachment and Sound
+
+From iOS 10.0 `User Notification` framework support to customize notification sound and notification can show attachments.
+
+__Limitation:__ 
+* Its support multiple notification but show first attachment only in notification alert.
+* Notification only support .aiff, .wav, or .caf sound file.
+
+
+### Category and Action
+
+`User Notification` support notification category. We must register our category objects using the `setNotificationCategories(_:)` method of `UNUserNotificationCenter`. Each notification category have associated action. Based on category system will show the appropriate action
+
+
+### Service extension
+
+Service extension give the opportunity to modify notification content before delivery to user. We can use this opportunity to modify notification content.
+Remote notification payload we must include `mutable-content: 1` within aps object. otherwise service extension not work.
+
+
+###  Content extension
+
+We can customize notification appearance using `UNNotificationContentExtension`. We shoud adopt this protocol in the custom UIViewController subclass that we use to present our interface
+
+
+### Conclusion
+
+This framework definitely do lot than what we saw here. If you want to learn more should checkout official apple documentation.
+
+[UserNotification](https://developer.apple.com/documentation/usernotifications)
---
+++ Foodie/Foodie/ViewController.swift
@@ -0,0 +1,194 @@
+//
+//  ViewController.swift
+//  Foodie
+//
+//  Created by Anbalagan D on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var generateNotificationButton: UIButton!
+    // Localized String in Notification
+    //class func localizedUserNotificationString(forKey key: String, arguments: [Any]?) -> String
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        generateNotificationButton.layer.borderWidth = 1.0
+        generateNotificationButton.layer.borderColor = UIColor.red.cgColor
+        
+        registerNotificationCategory()
+        UNUserNotificationCenter.current().delegate = self
+        
+//        UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: ["test"])
+//
+//        UNUserNotificationCenter.current().getDeliveredNotifications { notifications in
+//            for notification in notifications {
+//                notification.request.identifier
+//            }
+//        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        var authorizationOption: UNAuthorizationOptions = [.alert, .sound, .badge] //, .carPlay]
+        
+        if #available(iOS 12.0, *) {
+//            authorizationOption.insert(.provisional)
+//            authorizationOption.insert(.criticalAlert)
+            authorizationOption.insert(.providesAppNotificationSettings)
+        }
+        
+        if #available(iOS 13.0, *) {
+            authorizationOption.insert(.announcement)
+        }
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: authorizationOption) { isAuthorized, error in
+            print(isAuthorized)
+            print(error ?? "")
+            
+            if isAuthorized {
+//                DispatchQueue.main.async {
+//                    UIApplication.shared.registerForRemoteNotifications()
+//                }
+                return
+            }
+            
+//            DispatchQueue.main.async {
+//                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+//            }
+        }
+    }
+    
+    @IBAction private func generateNotificationTapped() {
+//        UNUserNotificationCenter.current().getNotificationSettings { notificationSettings in
+//            print(notificationSettings)
+//        }
+        
+        generateNotificationButton.layer.borderWidth = 1.0
+        generateNotificationButton.layer.borderColor = UIColor.green.cgColor
+        
+        scheduleNotification()
+    }
+    
+    private func scheduleNotification() {
+        //Create Notification Content
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = "Test notification created by anbu"
+        notificationContent.subtitle = "Fun with notification"
+        notificationContent.body = "Keep learn by your mistake and don't repeat that again. :)"
+        notificationContent.badge = 1
+        notificationContent.sound = UNNotificationSound(named: UNNotificationSoundName("cucko.wav"))
+        notificationContent.categoryIdentifier = "user_action_category"
+        if #available(iOS 15.0, *) {
+            notificationContent.interruptionLevel = .timeSensitive
+        }
+
+        if #available(iOS 12.0, *) {
+            let isFavorite = Bool.random()
+            notificationContent.threadIdentifier = isFavorite ? "Favorite food" : "Recently order"
+            notificationContent.summaryArgument = isFavorite ? "Favorite" : "Recent"
+            notificationContent.summaryArgumentCount = 56
+        }
+        
+        let foodUrl = Bundle.main.url(forResource: "food", withExtension: "jpeg")
+        let salt_lemon = Bundle.main.url(forResource: "salt_lemon", withExtension: "jpeg")
+        
+        let url = Bool.random() ? foodUrl : salt_lemon
+        if let attachment = try? UNNotificationAttachment(
+            identifier: UUID().uuidString,
+            url: url!,
+            options: nil
+        ) {
+            notificationContent.attachments = [attachment]
+        }
+        
+        //Create Trigger
+        let timeIntervalTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        //Create Notification request
+        let notificationRequest = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: notificationContent,
+            trigger: timeIntervalTrigger
+        )
+        
+        //Schedule notification with the system
+        UNUserNotificationCenter.current().add(notificationRequest) { error in
+            print(error ?? "")
+        }
+    }
+    
+    private func registerNotificationCategory() {
+        
+        let commentAction = UNTextInputNotificationAction(
+            identifier: "comment_action",
+            title: "Comment",
+            options: UNNotificationActionOptions(rawValue: 0),
+            textInputButtonTitle: "Comment",
+            textInputPlaceholder: "Enter your comment about this quote"
+        )
+        
+        let likeAction = UNNotificationAction(
+            identifier: "like_action",
+            title: "Like",
+            options: UNNotificationActionOptions(rawValue: 0)
+        )
+        
+        let dislikeAction = UNNotificationAction(
+            identifier: "dislike_action",
+            title: "Dislike",
+            options: UNNotificationActionOptions(rawValue: 0)
+        )
+        
+        if #available(iOS 11.0, *) {
+            let notificationCategory = UNNotificationCategory(
+                identifier: "user_action_category",
+                actions: [commentAction, likeAction, dislikeAction],
+                intentIdentifiers: [],
+                options: [.hiddenPreviewsShowSubtitle, .hiddenPreviewsShowTitle]
+            )
+            
+//            UNUserNotificationCenter.current().setNotificationCategories([notificationCategory])
+            
+            let testNotificationCategory = UNNotificationCategory(
+                identifier: "test_action_category",
+                actions: [commentAction, likeAction, dislikeAction],
+                intentIdentifiers: ["Hai", "Hello"],
+                options: [.hiddenPreviewsShowSubtitle, .hiddenPreviewsShowTitle]
+            )
+            
+            UNUserNotificationCenter.current().setNotificationCategories([notificationCategory, testNotificationCategory])
+            
+        } else {
+            let notificationCategory = UNNotificationCategory(
+                identifier: "user_action_category",
+                actions: [commentAction, likeAction, dislikeAction],
+                intentIdentifiers: [],
+                options: .customDismissAction
+            )
+            
+            UNUserNotificationCenter.current().setNotificationCategories([notificationCategory])
+        }
+    }
+}
+
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?) {
+        print(#function)
+    }
+}
+
+
